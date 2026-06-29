31d38c7544373877c183be6af9c2d1a0a13d06ce
Priyanshu Singh <sbhola833@gmail.com>
2026-06-29 22:54:24 +0530

    feat: add TMDB query hook for fetching movies and  with category and with search for movie feature

--- frontend/src/features/movies/api/tmdbService.js
+++ frontend/src/features/movies/api/tmdbService.js
@@ -1,7 +1,16 @@
 import tmdbApi from "../../../api/axios";
 
-export const fetchTrendingMovies = async () => {
-  const { data } = await tmdbApi.get('/trending/movie/day');
+export const fetchTrendingMovies = async (page=1) => {
+  const {data} = await tmdbApi.get(`/trending/movie/day`,{params:{page:page}});
   return data.results;
 };
 
+export const fetchCategoriesMovies = async (category="popular",page=1) => {
+  const {data} = await tmdbApi.get(`/movie/${category}`,{params:{page:page}});
+  return data.results;
+};
+
+export const featchSearchMovies = async (input,page=1) => {
+  const {data} = await tmdbApi.get(`/search/movie`,{params:{ query:input,page:page}});
+  return data.results;
+}
--- frontend/src/features/movies/hooks/useMovies.js
+++ frontend/src/features/movies/hooks/useMovies.js
@@ -1,13 +1,32 @@
 // src/features/movies/hooks/useMovies.js
 import { useQuery } from '@tanstack/react-query';
-import { fetchTrendingMovies } from '../api/tmdbService';
+import { fetchTrendingMovies,fetchCategoriesMovies,featchSearchMovies } from '../api/tmdbService';
 
-export const useTrendingMovies = () => {
+export const useTrendingMovies = (page=1) =>{
   return useQuery({
-    queryKey: ['trendingMovies'],
-    queryFn: fetchTrendingMovies,
-    staleTime: 1000 * 60 * 60 , // Data stays fresh for 10 sec
-  });
+    queryKey: ['trendingMovies',page],
+    queryFn: () => fetchTrendingMovies(page),
+    staleTime: 1000 * 60,
+    gcTime: 1000 * 60 ,
+  })
+};
+
+export const useCategoriesMovies = (category="popular",page=1) => {
+  return useQuery({
+    queryKey: ['movies',category,page],
+    queryFn: () => fetchCategoriesMovies(category,page),
+    staleTime: 1000 * 60,
+    gcTime: 1000 * 60 ,
+
+  })
+};
+
+export const useSearchMovies = (input,page=1) => {
+  return useQuery({
+    queryKey: ['search',input],
+    queryFn: () => featchSearchMovies(input,page),
+    gcTime: 1000 *10 ,
+  })
 };
 
 
