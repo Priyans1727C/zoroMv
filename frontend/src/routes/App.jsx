e4eb470911a96e3f8c7a26ecc371d1f422d3d254
Priyanshu Singh <sbhola833@gmail.com>
2026-08-02 06:27:52 +0530

    fix: remove the unwanted import for routes

--- frontend/src/routes/App.jsx
+++ frontend/src/routes/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import MainLayout from "../layouts/MainLayout";
-import TestLayout from "../features/test/TestLayout";
 import Home from "../features/home/Home";
 import MovieDetails from "../features/shared/pages/CardDetails";
 import Player from "../features/shared/pages/Player";
 import SearchPage from "../features/shared/pages/Search";
-import StickyCardStack from "../features/shared/pages/Test";
+
 import Browse from "../features/shared/pages/Browse";
-import Test from "../features/shared/pages/Test";
 import ComingSoonPage from "../features/shared/components/CommingSoon";
 import { Compass, Heart, User, Settings } from "lucide-react";
 
